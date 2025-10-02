b58132be769cdef47a1c62bcd1ad0c0c2bcbdec5
duduclaza <duduclaza@gmail.com>
2025-10-02 03:28:22 -0300

    fix: update vendor path and simplify email test message

--- public/test-email.php
+++ public/test-email.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -7,23 +7,20 @@ use PHPMailer\PHPMailer\Exception;
 $mail = new PHPMailer(true);
 
 try {
-    // Config SMTP
     $mail->isSMTP();
     $mail->Host       = 'smtp.hostinger.com';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'djsgqoti@sgqoti.com.br';
     $mail->Password   = 'Pandora@1989';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // SSL
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;
 
-    // Remetente e destinatário
     $mail->setFrom('djsgqoti@sgqoti.com.br', 'SGQ OTI DJ');
-    $mail->addAddress('seuemail@gmail.com'); // troque pelo seu email para teste
+    $mail->addAddress('seuemail@gmail.com'); // troque pelo seu email
 
-    // Conteúdo
     $mail->isHTML(true);
     $mail->Subject = 'Teste PHPMailer + Hostinger';
-    $mail->Body    = 'Se você recebeu este e-mail, sua configuração SMTP está ok!';
+    $mail->Body    = 'Funcionou! Se você recebeu este email, está OK.';
 
     $mail->send();
     echo '✅ E-mail enviado com sucesso!';
