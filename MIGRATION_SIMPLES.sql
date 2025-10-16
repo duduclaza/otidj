4eb3bd6664b7c8f057f8a93fe897dde9bd6a5951
duduclaza <duduclaza@gmail.com>
2025-10-15 22:44:22 -0300

    feat: add admin-only download button in flowchart visualizations tab

--- MIGRATION_SIMPLES.sql
+++
@@ -1,7 +0,0 @@
-/* ===== MIGRATION SIMPLES: Adicionar coluna notificacoes_ativadas ===== */
-/* Execute esta versão simplificada no phpMyAdmin */
-
-ALTER TABLE users 
-ADD COLUMN notificacoes_ativadas TINYINT(1) NOT NULL DEFAULT 1 
-COMMENT '1 = Notificações ativadas, 0 = Notificações desativadas' 
-AFTER status;
