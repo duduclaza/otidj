3c982b22124fd37483b4299b46c2aca75bc7069d
duduclaza <duduclaza@gmail.com>
2025-11-28 00:45:06 -0300

    refactored database connection initialization in clientes controller to use singleton instance directly

--- src/Controllers/ClientesController.php
+++ src/Controllers/ClientesController.php
@@ -10,7 +10,7 @@ class ClientesController
 
     public function __construct()
     {
-        $this->db = Database::getInstance()->getConnection();
+        $this->db = Database::getInstance();
     }
 
     /**
